5ddd1d128f4b78e26b054e6a70047f65ec8bfb7c
Jade <jarethmtz3005@gmail.com>
2025-05-11 20:07:13 -0600

    Se agregó el login

--- app/src/main/java/com/finpofe/habits/LoginActivity.kt
+++ app/src/main/java/com/finpofe/habits/LoginActivity.kt
@@ -1,12 +1,32 @@
 package com.finpofe.habits
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.credentials.GetCredentialRequest
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var emailEdt: EditText
+    private lateinit var passEdt: EditText
+    private lateinit var loginBtn: Button
+    private lateinit var googlebtn: ImageView
+    private lateinit var registerbtn: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +36,41 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        auth = Firebase.auth
+        emailEdt = findViewById(R.id.email_txt)
+        passEdt = findViewById(R.id.password_txt)
+        loginBtn = findViewById(R.id.login_Btn)
+        googlebtn = findViewById(R.id.google_img)
+        registerbtn = findViewById(R.id.register_lbl)
+
+        loginBtn.setOnClickListener {
+            val emailString = emailEdt.text.toString()
+            val passString = passEdt.text.toString()
+
+            auth.signInWithEmailAndPassword(emailString, passString).addOnCompleteListener (this) { task ->
+                if(task.isSuccessful) {
+                    Toast.makeText(baseContext, "Inicio de sesion exitoso", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else{
+                    Toast.makeText(baseContext, "Fallo al iniciar sesion", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        googlebtn.setOnClickListener {
+            val googleIdOption = GetGoogleIdOption.Builder()
+                .setServerClientId(getString(R.string.id_client_web))
+                .setFilterByAuthorizedAccounts(true)
+                .build()
+            val request = GetCredentialRequest.Builder()
+                .addCredentialOption(googleIdOption)
+                .build()
+        }
+
+        registerbtn.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
     }
 }
--- app/src/main/java/com/finpofe/habits/SplashActivity.kt
+++ app/src/main/java/com/finpofe/habits/SplashActivity.kt
@@ -8,21 +8,42 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
+
+    //Instancia de FirebaseAuth
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         val splashScreen = installSplashScreen() //Se guarda la referencia del splash
         super.onCreate(savedInstanceState)
+
+        //Se inicializa auth
+        auth = Firebase.auth
 
         //Mantener splash abierto hasta que este listo lo demas
         splashScreen.setKeepOnScreenCondition { true }
 
+        //Referencia al usuario actual
+        val currentUser = auth.currentUser
+
         //Splash abierto hasta que se cumpla esto
-        lifecycleScope.launch{
-            delay(2000) //En lugar de delay, cargar datos xd
-            startActivity(Intent(this@SplashActivity, LoginActivity::class.java)) //Se inicia la Main
+        lifecycleScope.launch {
+            //Checa si hay usuario iniciado
+            if (currentUser != null){
+                //Se inicia Main si hay usuario iniciado
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            }else{
+                //Si no hay usuario inciado, manda a iniciar sesion
+                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            }
+
             finish() //Finaliza la activity
         }
     }
